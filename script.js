a76a2d34fe74337673265b7e1b4222525d737a9c
Shahed1998 <shahedchowdhury120@gmail.com>
2020-12-30 15:12:55 +0600

    third commit

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.querySelector('#check').addEventListener('click', function () {
   document.querySelector('#check').textContent = 'Lets begin ✔ ';
   document.querySelector('#form-fill').classList.remove('hidden');
 });
+
+// complete the dynamically built form
+document.querySelector('#click').addEventListener('click', function () {
+  const num = Number(document.querySelector('#Num').value);
+  const arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push(i + 1);
+  }
+  document.querySelector('#playerNames').textContent = arr;
+});
